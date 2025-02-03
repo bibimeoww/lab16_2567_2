40fc98f5d8e222080dfeffe89b07620f2d411328
bibimeoww <papitchaya2404@gmail.com>
2025-02-03 15:49:12 +0700

    yes

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -4,13 +4,38 @@
 #include<iomanip>
 using namespace std;
 
-void showData(double *,int,int);
+void showData(double* arr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(arr+i);
+        if(i % M == M - 1) cout << endl;
+        else cout << " ";
+    }
+}
 
-void randData(double *,int,int);
+void randData(double* arr, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        *(arr+i) = (rand() % 101) * 0.01;
+    }
+}
 
-void findRowSum(const double *,double *,int,int);
+void findRowSum(const double *arr, double *sum, int N, int M) {
+    for (int i = 0; i < N; i++) {
+        sum[i] = 0;
+        for (int j = 0; j < M; j++) {
+            sum[i] += arr[i * M + j];
+        }
+    }
+}
 
-void findColSum(const double *,double *,int,int);
+void findColSum(const double *arr, double *sum, int N, int M) {
+    for (int j = 0; j < M; j++) {
+        sum[j] = 0;
+        for (int i = 0; i < N; i++) {
+            sum[j] += arr[i * M + j];
+        }
+    }
+}
 
 int main(){
 	srand(time(0));
